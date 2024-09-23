6c16c7ef892d1e91f7707bb88d189dec9a8b324e
gremble0 <hermanstornes@gmail.com>
2024-09-23 10:50:31 +0200

    Complete extension

--- src/extensions/nested-loops.js
+++ src/extensions/nested-loops.js
@@ -29,16 +29,43 @@ for (let i = START; i <= END; ++i) {
   for (let j = START - 1, value = i; j < i; ++j, --value) builder[j] = value
   nestedTwo.push(builder)
 }
-console.log(nestedTwo)
 
 // 4. As 2, but each array should contain arrays from 1 to the outer 'loop index' with the value of the outer 'loop index'. Update array 'deepOne'
 //    eg [[[1]],[[2],[2,2]],...]
+for (let i = START; i <= END; i++) {
+  let builder = new Array()
+  for (let j = START; j <= i; j++) {
+    builder.push(new Array(j).fill(i))
+  }
+  deepOne.push(builder)
+}
 
 // 5. As 4, update array 'deepTwo' so that the result is:
 //       [[[1]],[[1],[1,2]],...]
+for (let i = START; i <= END; i++) {
+  let outerArray = new Array()
+  for (let j = START; j <= i; j++) {
+    let innerArray = new Array()
+    for (let k = START; k <= j; k++) innerArray.push(k)
+    outerArray.push(innerArray)
+  }
+  deepTwo.push(outerArray)
+}
 
 // 6. As 5, update the array 'deepThree', but the result should be the average of the sum of the squares of the numbers in each array
 //       [[1],[[1],[2.5]],...]
+for (let i = START; i <= END; i++) {
+  let outerArray = new Array()
+  for (let j = START; j <= i; j++) {
+    let sum = 0
+    for (let k = START; k <= j; k++) {
+      sum += k * k
+    }
+    let average = sum / j
+    outerArray.push([average])
+  }
+  deepThree.push(outerArray)
+}
 
 module.exports = {
   START,
